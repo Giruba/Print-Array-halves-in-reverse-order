910e1be0e7eb1ede38d5a6df48a73cec2caa8322
Giruba Beulah SE <gibeulah@microsoft.com>
2019-05-09 02:25:40 +0530

    Add project files.

---
+++ SplitAnArayIntoTwoHalves/ArrayUtility.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SplitAnArayIntoTwoHalves
+{
+    class ArrayUtility
+    {
+        public static int[] GetArrayFromUserInput() {
+            int[] array = null;
+
+            Console.WriteLine("Enter the number of elements in the array");
+            try
+            {
+                int numberElemts = int.Parse(Console.ReadLine().Trim());
+                array = new int[numberElemts];
+                Console.WriteLine("Enter the elements of the array separated " +
+                    "by space, comma or semi-colon");
+                String[] elements = Console.ReadLine().Trim().Split(' ',',',';');
+                for (int index = 0; index < numberElemts; index++) {
+                    array[index] = int.Parse(elements[index]);
+                }
+            }
+            catch (Exception exception) {
+                Console.WriteLine("Thrown exception is "+exception.Message);
+            }
+
+            return array;
+        }
+
+        public static void PrintArrayHalves(int[] array) {
+
+            int sizeBy2 = array.Length / 2;
+            //Print Second Half
+            Console.WriteLine("Printing second half...");
+            for (int index = sizeBy2; index < array.Length; index++) {
+                Console.Write(array[index]+ " ");
+            }
+
+            Console.WriteLine();
+
+            //Print First Half
+            Console.WriteLine("Printing first half...");
+            for (int index = 0; index < sizeBy2; index++)
+            {
+                Console.Write(array[index]+ " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ SplitAnArayIntoTwoHalves/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SplitAnArayIntoTwoHalves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Split an array into two-halves and print them!");
+            Console.WriteLine("----------------------------------------------");
+            int[] array = ArrayUtility.GetArrayFromUserInput();
+            ArrayUtility.PrintArrayHalves(array);
+            Console.ReadLine();
+        }
+    }
+}
